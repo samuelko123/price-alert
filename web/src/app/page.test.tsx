3658f99f71bca372144ec249ea85a86b38dea478
Samuel Ko <koshunyin@gmail.com>
2024-12-15 18:39:42 +1100

    ensure the test can fail

--- web/src/app/page.test.tsx
+++ web/src/app/page.test.tsx
@@ -5,7 +5,7 @@ describe('Page', () => {
   it('displays Hello World', () => {
     render(<Page />);
 
-    const element = screen.getByText("Hello World");
+    const element = screen.getByText("Hello Worl");
 
     expect(element).toBeVisible();
   })
