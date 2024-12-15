16f6ffe48bbb459a2302c8a60e315a53a50fbc93
Samuel Ko <koshunyin@gmail.com>
2024-12-15 22:22:59 +1100

    remove swagger

--- backend/PriceAlert/API/Program.cs
+++ backend/PriceAlert/API/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 internal class Program
 {
@@ -10,19 +8,7 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder();
 
-        // Add services to the container.
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
 
         var summaries = new[]
         {
@@ -40,9 +26,7 @@ internal class Program
                 ))
                 .ToArray();
             return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
+        });
 
         app.Run();
     }
