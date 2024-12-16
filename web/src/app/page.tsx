dbd403e82a6ff254bb08ba1c4588f862dd2e3c5c
Samuel Ko <koshunyin@gmail.com>
2024-12-16 22:10:52 +1100

    display dummy product on front end

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+// this line can be removed once the page becomes dynamic, i.e. with url path parameter
+export const dynamic = 'force-dynamic'
+
+export default async function Home() {
+  const res = await fetch('http://api:5000/api/products/1');
+  const product = await res.json();
+
   return (
     <main>
-      Hello World
+      <h1>Hello World</h1>
+      <p>
+        <a href={product.url}>{product.name}</a>
+      </p>
     </main>
   );
 }
