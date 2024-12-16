d14001679e32c8779dcb3d84856001ec0a3f9100
Samuel Ko <koshunyin@gmail.com>
2024-12-17 09:57:21 +1100

    use `toBeVisible` assertion

--- web/src/app/page.test.tsx
+++ web/src/app/page.test.tsx
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom/vitest';
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Page from './page';
@@ -7,5 +8,5 @@ test('Page', () => {
 
   const element = screen.getByText("Hello World");
 
-  expect(element).toBeDefined();
+  expect(element).toBeVisible();
 })
