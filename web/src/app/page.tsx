f1656b3e315c232024f3deeb635a74f78149c1f6
Samuel Ko <koshunyin@gmail.com>
2024-12-17 00:38:33 +1100

    move data fetching to a separate page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,7 @@
-// this line can be removed once the page becomes dynamic, i.e. with url path parameter
-export const dynamic = 'force-dynamic'
-
-export default async function Home() {
-  const res = await fetch('http://api:5000/api/products/1');
-  const product = await res.json();
-
+export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
-      <p>
-        <a href={product.url}>{product.name}</a>
-      </p>
     </main>
   );
 }
