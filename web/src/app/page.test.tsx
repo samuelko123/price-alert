24cd0e8802acd2922613d0190ec4df3c74f623f2
Samuel Ko <koshunyin@gmail.com>
2024-12-17 20:45:10 +1100

    add `describe` to tests

--- web/src/app/page.test.tsx
+++ web/src/app/page.test.tsx
@@ -1,11 +1,13 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Page from './page';
 
-test('Page', () => {
-  render(<Page />)
+describe('Home Page', () => {
+  it('displays Hello World', () => {
+    render(<Page />)
 
-  const element = screen.getByText("Hello World");
+    const element = screen.getByText("Hello World");
 
-  expect(element).toBeVisible();
-})
+    expect(element).toBeVisible();
+  });
+});
--- web/src/app/products/[id]/page.test.tsx
+++ web/src/app/products/[id]/page.test.tsx
@@ -1,12 +1,14 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Page from './page';
 
-test('Page', async () => {
-  const component = await Page();
-  
-  render(component);
+describe('Product Page', () => {
+  test('displays product name', async () => {
+    const component = await Page();
 
-  const element = screen.getByRole("link", { name: "A dummy product" });
-  expect(element).toBeVisible();
-})
+    render(component);
+
+    const element = screen.getByRole("link", { name: "A dummy product" });
+    expect(element).toBeVisible();
+  })
+});
