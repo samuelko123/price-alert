6ee4d7ec2bd0b5d1ab8ab1244b3142a806c3c14d
Samuel Ko <koshunyin@gmail.com>
2024-12-17 18:46:20 +1100

    make `page.test.tsx` look better

--- web/src/app/products/[id]/page.test.tsx
+++ web/src/app/products/[id]/page.test.tsx
@@ -3,9 +3,10 @@ import { render, screen } from '@testing-library/react';
 import Page from './page';
 
 test('Page', async () => {
-  render(await Page());
+  const component = await Page();
+  
+  render(component);
 
   const element = screen.getByRole("link", { name: "A dummy product" });
-
   expect(element).toBeVisible();
 })
