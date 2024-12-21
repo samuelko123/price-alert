1980ec92c32e051fda5cca1da6ea3c3dae7d2eef
Samuel Ko <koshunyin@gmail.com>
2024-12-21 22:05:00 +1100

    remove `prefer-const` as it is included in `eslint`

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "prefer-const": "error"
-    }
-  }
 ];
 
 export default eslintConfig;
