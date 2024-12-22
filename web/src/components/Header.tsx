692430996499aa5e9893fde0023879ac0e4518a9
Samuel Ko <koshunyin@gmail.com>
2024-12-23 09:49:54 +1100

    configure background colour of Header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 ">
+      <nav className="bg-surface px-4 py-2">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a href="https://flowbite.com" className="flex items-center">
             <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
