6be4d1c408a75055fce6ffc0707f3ada95d14dd6
Samuel Ko <koshunyin@gmail.com>
2024-12-23 10:01:51 +1100

    update logo

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/logo.png";
+
 export const Header = () => {
   return (
     <header>
       <nav className="bg-surface px-4 py-2">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="https://flowbite.com" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <Link href="/" className="flex items-center">
+            <Image src={Logo} width="40" height="40" alt="App Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap ">Flowbite</span>
-          </a>
+          </Link>
           <div className="flex items-center lg:order-2">
             <a href="#" className="text-gray-800  hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2  focus:outline-none ">Log in</a>
             <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2   focus:outline-none ">Get started</a>
