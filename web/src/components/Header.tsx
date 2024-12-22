a3123971e60db9865282d5a9dfabffcadd7e04ce
Samuel Ko <koshunyin@gmail.com>
2024-12-23 10:08:44 +1100

    remove `<nav>` element

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -4,13 +4,11 @@ import Logo from "@/public/logo.png";
 
 export const Header = () => {
   return (
-    <header>
-      <nav className="bg-surface px-4 py-2 flex flex-wrap justify-between items-center">
-        <Link href="/" className="flex items-center gap-1">
-          <Image src={Logo} width="40" height="40" alt="App Logo" />
-          <span className="text-xl font-semibold whitespace-nowrap">Price Alert</span>
-        </Link>
-      </nav>
+    <header className="bg-surface px-4 py-2 flex flex-wrap justify-between items-center">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src={Logo} width="40" height="40" alt="App Logo" />
+        <span className="text-xl font-semibold whitespace-nowrap">Price Alert</span>
+      </Link>
     </header>
   );
 };
