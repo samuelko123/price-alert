361a7537e5d1f6854a869052da2a3aa968aa1f0b
Samuel Ko <koshunyin@gmail.com>
2024-12-24 01:30:50 +1100

    return product name from API response

--- backend/src/PriceAlert/Infrastructure/Woolworths/WoolworthsApiClient.cs
+++ backend/src/PriceAlert/Infrastructure/Woolworths/WoolworthsApiClient.cs
@@ -1,16 +1,21 @@
 using System.Net.Http;
+using System.Threading.Tasks;
 using PriceAlert.Domain;
 
 namespace PriceAlert.Infrastructure.Woolworths;
 
 public class WoolworthsApiClient(HttpClient httpClient)
 {
-  public Product GetProduct(string id)
+  public async Task<Product> GetProduct(string id)
   {
+    var url = $"https://www.woolworths.com.au/api/v3/ui/schemaorg/product/{id}";
+    var response = await httpClient.GetAsync(url);
+    var content = await response.Content.ReadAsStringAsync();
+
     return new Product()
     {
       Id = id,
-      Name = "a product name",
+      Name = content,
     };
   }
 }
--- backend/tests/PriceAlert.UnitTests/Infrastructure/Woolworths/WoolworthsApiClientTest.cs
+++ backend/tests/PriceAlert.UnitTests/Infrastructure/Woolworths/WoolworthsApiClientTest.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using FakeItEasy;
 using PriceAlert.Infrastructure.Woolworths;
 
@@ -8,7 +9,7 @@ namespace PriceAlert.UnitTests.Infrastructure.Woolworths;
 public class WoolworthsProductApiClientTest
 {
   [Fact]
-  public void GetProduct_ReturnsProduct()
+  public async Task GetProduct_ReturnsProductAsync()
   {
     // Arrange
     var messageHandler = A.Fake<HttpMessageHandler>();
@@ -23,18 +24,18 @@ public class WoolworthsProductApiClientTest
     var apiClient = new WoolworthsApiClient(httpClient);
 
     // Action
-    var product = apiClient.GetProduct("123");
+    var product = await apiClient.GetProduct("123");
 
     // Assert
     Assert.Equal("123", product.Id);
     Assert.Equal("product name from Woolworths", product.Name);
   }
 
-  private static void SetResponse(HttpMessageHandler messageHandler, HttpResponseMessage response)
+  private void SetResponse(HttpMessageHandler messageHandler, HttpResponseMessage response)
   {
     A.CallTo(messageHandler)
-          .Where(x => x.Method.Name == "SendAsync")
-          .WithReturnType<HttpResponseMessage>()
-          .Returns(response);
+      .Where(x => x.Method.Name == "SendAsync")
+      .WithReturnType<Task<HttpResponseMessage>>()
+      .Returns(response);
   }
 }
