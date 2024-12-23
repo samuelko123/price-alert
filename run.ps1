1c27c64e792195263743d828cff4dd6a1bf5f126
Samuel Ko <koshunyin@gmail.com>
2024-12-23 16:25:37 +1100

    extract `RunCommand`

--- run.ps1
+++ run.ps1
@@ -5,14 +5,20 @@ param(
   [string]$profile
 )
 
+function RunCommand([string]$command) {
+  Write-Host "Running the following command:"
+  Write-Host $command
+  Invoke-Expression $command
+}
+
 switch ($profile) {
   "dev" {
-    Invoke-Expression "docker compose --file ./infra/docker/docker-compose.yaml --file ./infra/docker/docker-compose.dev.yaml up --build --remove-orphans"
+    RunCommand("docker compose --file ./infra/docker/docker-compose.yaml --file ./infra/docker/docker-compose.dev.yaml up --build --remove-orphans")
   }
   "prod" {
-    Invoke-Expression "docker compose --file ./infra/docker/docker-compose.yaml up --build --remove-orphans"
+    RunCommand("docker compose --file ./infra/docker/docker-compose.yaml up --build --remove-orphans")
   }
   "test" {
-    Invoke-Expression "docker compose --file ./infra/docker/docker-compose.test.yaml up --build --remove-orphans"
+    RunCommand("docker compose --file ./infra/docker/docker-compose.test.yaml up --build --remove-orphans")
   }
 }
