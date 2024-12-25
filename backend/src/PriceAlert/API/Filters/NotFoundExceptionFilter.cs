f15e01c02692bcf5857f0af60fbdb6b5d0b538c1
Samuel Ko <koshunyin@gmail.com>
2024-12-25 22:10:06 +1100

    handle `ProductNotFoundException`

---
+++ backend/src/PriceAlert/API/Filters/NotFoundExceptionFilter.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PriceAlert.API.Errors;
+using PriceAlert.Infrastructure.Exceptions;
+
+namespace PriceAlert.API.Filters;
+
+public class NotFoundExceptionFilter() : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+        if (exception is ProductNotFoundException)
+        {
+            context.Result = new NotFoundObjectResult(new Error(exception.Message));
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- backend/src/PriceAlert/Program.cs
+++ backend/src/PriceAlert/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PriceAlert.API.Filters;
 using PriceAlert.Domain;
 using PriceAlert.Infrastructure.Woolworths;
 
@@ -10,7 +11,10 @@ internal class Program
     private static void Main()
     {
         var builder = WebApplication.CreateBuilder();
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add<NotFoundExceptionFilter>();
+        });
         builder.Services.AddSingleton<IProductRepository, ProductRepository>();
         builder.Services.AddSingleton<IWoolworthsApiClient, WoolworthsApiClient>();
         builder.Services.AddHttpClient<WoolworthsApiClient>();
