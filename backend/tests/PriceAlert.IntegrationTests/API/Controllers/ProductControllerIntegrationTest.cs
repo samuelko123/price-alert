4a3199b0b211fa1f4b84ac9a146608c77aa38050
Samuel Ko <koshunyin@gmail.com>
2024-12-25 16:26:08 +1100

    create failed test

--- backend/tests/PriceAlert.IntegrationTests/API/Controllers/ProductControllerIntegrationTest.cs
+++ backend/tests/PriceAlert.IntegrationTests/API/Controllers/ProductControllerIntegrationTest.cs
@@ -9,21 +9,6 @@ namespace PriceAlert.IntegrationTests.API.Controllers;
 public class ProductControllerIntegrationTest
 {
   [Fact]
-  public async Task Search_WithMissingRequiredProperty_ReturnsBadRequest()
-  {
-    // Arrange
-    using var factory = new BaseWebApplicationFactory();
-    using var client = factory.CreateClient();
-
-    // Action
-    var response = await client.PostAsync("/api/products/getByUrl", new StringContent("{}", Encoding.UTF8, "application/json"));
-
-    // Assert
-    Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-    Assert.Equal("""{"error":"missing required property: 'url'."}""", await response.Content.ReadAsStringAsync());
-  }
-
-  [Fact]
   public async Task Search_WithValidBody_ReturnsOk()
   {
     // Arrange
@@ -31,7 +16,7 @@ public class ProductControllerIntegrationTest
     using var client = factory.CreateClient();
 
     // Action
-    var response = await client.PostAsync("/api/products/getByUrl", new StringContent("""{"url": "https://www.woolworths.com.au/shop/productdetails/123"}""", Encoding.UTF8, "application/json"));
+    var response = await client.GetAsync("/api/products/getByUrl?url=https://www.woolworths.com.au/shop/productdetails/123");
 
     // Assert
     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
