ad15e823389cb6543bfcf1c4bd10f1ef981d3283
Samuel Ko <koshunyin@gmail.com>
2024-12-25 23:01:42 +1100

    rearrange test method ordering

--- backend/tests/PriceAlert.IntegrationTests/API/Controllers/ProductControllerIntegrationTest.cs
+++ backend/tests/PriceAlert.IntegrationTests/API/Controllers/ProductControllerIntegrationTest.cs
@@ -28,6 +28,20 @@ public class ProductControllerIntegrationTest
   }
 
   [Fact]
+  public async Task GetByUrl_WithProductUrl_ReturnsOk()
+  {
+    // Arrange
+    using var factory = new BaseWebApplicationFactory();
+    using var client = factory.CreateClient();
+
+    // Action
+    var response = await client.GetAsync("/api/products/getByUrl?url=https://www.woolworths.com.au/shop/productdetails/123");
+
+    // Assert
+    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+  }
+
+  [Fact]
   public async Task GetByUrl_WithProductNotFoundException_ReturnsStatusNotFound()
   {
     // Arrange
@@ -50,19 +64,5 @@ public class ProductControllerIntegrationTest
     // Assert
     Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     Assert.Equal("""{"error":"Unable to find product: 123"}""", await response.Content.ReadAsStringAsync());
-  }
-
-  [Fact]
-  public async Task GetByUrl_WithProductUrl_ReturnsOk()
-  {
-    // Arrange
-    using var factory = new BaseWebApplicationFactory();
-    using var client = factory.CreateClient();
-
-    // Action
-    var response = await client.GetAsync("/api/products/getByUrl?url=https://www.woolworths.com.au/shop/productdetails/123");
-
-    // Assert
-    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
   }
 }
