300e135252f69fb95303567d1b75add51b4dd9fb
Samuel Ko <koshunyin@gmail.com>
2024-12-27 22:19:23 +1100

    remove `GeneratedRegex`

--- backend/src/PriceAlert/Domain/ProductRepository.cs
+++ backend/src/PriceAlert/Domain/ProductRepository.cs
@@ -5,12 +5,6 @@ using PriceAlert.Infrastructure.Woolworths;
 
 namespace PriceAlert.Domain;
 
-public static partial class ProductUrlRegex
-{
-  [GeneratedRegex(@"https://www.woolworths.com.au/shop/productdetails/([0-9]+)", RegexOptions.Compiled | RegexOptions.IgnoreCase)]
-  public static partial Regex Woolworths();
-}
-
 public class ProductRepository(IWoolworthsApiClient client) : IProductRepository
 {
   public async Task<Product> FindProductByUrl(string url)
@@ -32,7 +26,7 @@ public class ProductRepository(IWoolworthsApiClient client) : IProductRepository
 
   private static string ExtractProductSkuFromUrl(string url)
   {
-    var match = ProductUrlRegex.Woolworths().Match(url);
+    var match = Regex.Match(url, @"https://www.woolworths.com.au/shop/productdetails/([0-9]+)", RegexOptions.IgnoreCase);
     if (!match.Success)
     {
       throw new DataValidationException("The url is invalid. It should start with 'https://www.woolworths.com.au/shop/productdetails/'.");
