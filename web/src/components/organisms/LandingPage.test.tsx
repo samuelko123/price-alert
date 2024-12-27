9e66a257e0ba72b0b3974c3b516e43942a3960f2
Samuel Ko <koshunyin@gmail.com>
2024-12-28 02:10:24 +1100

    update `LandingPage`

--- web/src/components/organisms/LandingPage.test.tsx
+++ web/src/components/organisms/LandingPage.test.tsx
@@ -3,10 +3,10 @@ import { render, screen } from "@testing-library/react";
 import Page from "@/app/page";
 
 describe("Landing Page", () => {
-  it("displays placeholder message", () => {
+  it("displays Product Search Form", () => {
     render(<Page />);
 
-    const element = screen.getByText("Features will be coming soon.");
+    const element = screen.getByText("Product URL");
 
     expect(element).toBeVisible();
   });
--- web/src/components/organisms/LandingPage.tsx
+++ web/src/components/organisms/LandingPage.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/components/atoms/Header";
+import { ProductSearchForm } from "../molecules/ProductSearchForm";
+import { Surface } from "../atoms/Surface";
 
 export const LandingPage = () => {
   return (
     <>
       <Header />
       <main className="px-4 py-4">
-        <p>Features will be coming soon.</p>
+        <Surface>
+          <ProductSearchForm />
+        </Surface>
       </main>
     </>
   );
