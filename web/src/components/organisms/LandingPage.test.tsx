7389e3128196b65c4fec723f5fe06dc7eaea62fe
Samuel Ko <koshunyin@gmail.com>
2024-12-29 21:52:45 +1100

    add `ProductDetail` to `LandingPage`

--- web/src/components/organisms/LandingPage.test.tsx
+++ web/src/components/organisms/LandingPage.test.tsx
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Page from "@/app/page";
+import userEvent from "@testing-library/user-event";
+import { http, HttpResponse, server } from "../../../tests/setup-server";
 
 describe("Landing Page", () => {
   it("displays Product Search Form", () => {
@@ -12,12 +14,28 @@ describe("Landing Page", () => {
     expect(label).toBeVisible();
   });
 
-  it("displays Product Detail", () => {
-    // Act
+  it("displays Product Detail after search", async () => {
+    // Arrange
+    server.use(
+      http.get("/api/products/getByUrl", () => {
+        return HttpResponse.json({
+          sku: "123",
+          name: "A dummy product",
+        });
+      }),
+    );
+
+    const user = userEvent.setup();
     render(<Page />);
 
+    // Act
+    const textbox = screen.getByRole("textbox", { name: "Product URL" });
+    await user.type(textbox, "https://google.com");
+    const button = screen.getByRole("button");
+    await user.click(button);
+
     // Assert
-    const product = screen.getByText("The Best Product");
+    const product = screen.getByText("A dummy product");
     expect(product).toBeVisible();
   });
 });
--- web/src/components/organisms/LandingPage.tsx
+++ web/src/components/organisms/LandingPage.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { Header } from "@/components/atoms/Header";
 import { ProductSearchForm } from "../molecules/ProductSearchForm";
 import { Surface } from "../atoms/Surface";
 import { ProductDetail } from "../molecules/ProductDetail";
+import { getProductByUrl } from "@/api/productApi";
+import { useState } from "react";
+import { Product } from "@/types/Product";
 
 export const LandingPage = () => {
-  const product = {
-    sku: "123",
-    name: "The Best Product",
+  const [product, setProduct] = useState<Product>();
+
+  const handleSubmit = async (url: string) => {
+    const data = await getProductByUrl(url);
+    setProduct(data);
   };
 
   return (
@@ -14,11 +21,13 @@ export const LandingPage = () => {
       <Header />
       <main className="px-4 py-4 flex flex-col gap-4">
         <Surface>
-          <ProductSearchForm />
+          <ProductSearchForm onSubmit={handleSubmit} />
         </Surface>
-        <Surface>
-          <ProductDetail product={product} />
-        </Surface>
+        {product &&
+          <Surface>
+            <ProductDetail product={product} />
+          </Surface>
+        }
       </main>
     </>
   );
