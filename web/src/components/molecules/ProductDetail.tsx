070f45306d41c19b44218602bb96eda858df2b61
Samuel Ko <koshunyin@gmail.com>
2024-12-31 17:24:45 +1100

    display price in front end

--- web/src/components/molecules/ProductDetail.tsx
+++ web/src/components/molecules/ProductDetail.tsx
@@ -5,10 +5,16 @@ export const ProductDetail = ({
 }: {
   product: Product,
 }) => {
+  const formatter = new Intl.NumberFormat("en-AU", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  const price = formatter.format(
+    product.priceInCents / 100,
+  );
+
   return (
     <div>
       <p>{product.sku}</p>
       <p>{product.name}</p>
+      <p>${price}</p>
     </div>
   );
 };
