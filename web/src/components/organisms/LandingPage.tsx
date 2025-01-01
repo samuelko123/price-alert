86fc9f33c819b1127c153c1b6ab000ee36611023
Samuel Ko <koshunyin@gmail.com>
2025-01-01 16:46:48 +1100

    Feature - Add front-end messaging (#46)

--- web/src/components/organisms/LandingPage.tsx
+++ web/src/components/organisms/LandingPage.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Header } from "@/components/atoms/Header";
 
+import { Surface } from "../atoms/Surface";
 import { ProductSearchForm } from "../molecules/ProductSearchForm";
 
 const queryClient = new QueryClient();
@@ -12,7 +13,11 @@ export const LandingPage = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
-      <main className="max-w-sm m-auto px-4 py-4">
+      <main className="max-w-sm m-auto px-4 py-4 flex flex-col gap-4">
+        <Surface>
+          <p>Supported merchants:</p>
+          <p>- Officeworks</p>
+        </Surface>
         <ProductSearchForm />
       </main>
     </QueryClientProvider>
