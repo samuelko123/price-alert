ff17b66deba992cb053c20cba45726a117304298
Samuel Ko <koshunyin@gmail.com>
2025-01-01 16:43:55 +1100

    add front-end messaging

--- web/src/components/atoms/Header.tsx
+++ web/src/components/atoms/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
     <header className="bg-surface px-4 py-2 flex flex-wrap justify-between items-center">
       <Link href="/" className="flex items-center gap-1">
         <Image src={Logo} width="40" height="40" alt="App Logo" />
-        <span className="text-xl font-semibold whitespace-nowrap">Price Alert</span>
+        <span className="text-xl font-semibold whitespace-nowrap">Price Checker</span>
       </Link>
     </header>
   );
