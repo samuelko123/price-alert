daf363dda192e25097cc0ac2721451088110deec
Samuel Ko <koshunyin@gmail.com>
2025-01-01 15:42:26 +1100

    Revert "temporarily create a script that works locally"
    This reverts commit 458e0e5f0b1f00d5b2ac6d0f0528bcdf5689025f.

--- backend/src/PriceAlert/Program.cs
+++ backend/src/PriceAlert/Program.cs
@@ -1,5 +1,12 @@
-using System;
-using PriceAlert.Infrastructure.Kmart;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using PriceAlert.API.Problems;
+using PriceAlert.Domain;
+using PriceAlert.Domain.Exceptions;
+using PriceAlert.Infrastructure.Officeworks;
 
 namespace PriceAlert;
 
@@ -7,7 +14,48 @@ internal class Program
 {
     private static void Main()
     {
-        var text = KmartScraper.Scrape().Result;
-        Console.WriteLine(text);
+        var builder = WebApplication.CreateBuilder();
+        builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+        builder.Services.AddSingleton<IOfficeworksApiClient, OfficeworksApiClient>();
+        builder.Services.AddHttpClient<OfficeworksApiClient>();
+
+        builder.Services.AddHealthChecks();
+        builder.Services.AddProblemDetails(option =>
+        {
+            option.CustomizeProblemDetails = context =>
+            {
+                var exception = context.Exception;
+                if (exception is DataValidationException)
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    context.ProblemDetails = new BadRequestProblemDetails(exception.Message);
+                }
+
+                if (exception is ItemNotFoundException)
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    context.ProblemDetails = new NotFoundProblemDetails(exception.Message);
+                }
+            };
+        });
+        builder.Services.AddMvc()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errorMessages = context.ModelState.Values
+                        .SelectMany(value => value.Errors.Select(error => error.ErrorMessage))
+                        .ToList();
+
+                    return new BadRequestObjectResult(new BadRequestProblemDetails(errorMessages));
+                };
+            });
+
+        var app = builder.Build();
+        app.UseExceptionHandler();
+        app.MapHealthChecks("/api/healthcheck");
+        app.MapControllers();
+
+        app.Run();
     }
 }
